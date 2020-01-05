eb41eeef04f82b1870c033f9db3549da0afa613e
Fatemeh Mohammadzadeh <mofa1012@hs-karlsruhe.de>
2020-01-05 15:09:11 +0100

    add pubsub

--- src/functions/src/index.ts
+++ src/functions/src/index.ts
@@ -1,5 +1,10 @@
 import * as functions from 'firebase-functions';
 import *  as admin from 'firebase-admin';
+// Imports the Google Cloud client library
+import {PubSub} from '@google-cloud/pubsub';
+//
+// // Creates a client
+const pubsub = new PubSub();
 admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
@@ -60,6 +65,54 @@ export const ggt_history = functions.https.onRequest(async (request, response) =
           zahl1: doc.data()['zahl1'],
           zahl2: doc.data()['zahl2'],
           ergebnis: doc.data()['ergebnis'],
+          date: new Date(doc.data()['datetime'])
+        }
+        allDocs.push(h);
+      });
+      return allDocs;
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+    response.send(allHistories );
+});
+
+export const publisher = functions.https.onRequest(async (request, response) => {
+  const path = request.path;
+  if(!path) {
+    response.status(400).send("Keine Daten");
+  } else {
+    const topicName = 'my-topic';
+     const data = JSON.stringify({ nachricht: path });
+
+  //  Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+    const dataBuffer = Buffer.from(data);
+
+    const messageId = await pubsub.topic(topicName).publish(dataBuffer);
+
+    console.log(`Message ${messageId} published.`);
+    response.send('OK');
+  }
+});
+
+exports.subscriber = functions.pubsub.topic('my-topic').onPublish(async (message) => {
+    console.log(`Message ${message.json.nachricht} received.`);
+    const docRef = db.collection('nachrichten').doc();
+    await docRef.set({
+      nachricht: message.json.nachricht,
+      datetime: Date.now()
+    });
+    return;
+});
+
+export const processed = functions.https.onRequest(async (request, response) => {
+  const historiesRef = db.collection('nachrichten').orderBy('datetime', 'desc').limit(10);
+  const allHistories = await historiesRef.get()
+    .then(snapshot => {
+      let allDocs: any[] = [];
+      snapshot.forEach(doc => {
+        const h = {
+          nachricht: doc.data()['nachricht'],
           date: new Date(doc.data()['datetime'])
         }
         allDocs.push(h);
